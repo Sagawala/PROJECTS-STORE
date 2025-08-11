079d38b4cb04f4f7a82a188d39910f1ad01199b5
Sagawala <johnsagawala360@gmail.com>
2025-08-11 05:03:20 +0300

    V 0.2 project file

--- spice-tech-gas-cylinder-main/src/main/java/com/spicetech/gascylinderapp/Repository/NotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spicetech.gascylinderapp.Repository;
-
-
-import com.spicetech.gascylinderapp.Entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
